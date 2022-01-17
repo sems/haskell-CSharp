a87065d5fe35bc892824baa4b88a713798c6eac9
Sem <me@sems.dev>
2022-01-17 13:59:10 +0100

    testing purposes

--- src/CSharpGram.hs
+++ src/CSharpGram.hs
@@ -90,6 +90,7 @@ pExprSimple =  ExprConstInt  <$> sConstI
 -- pExpr = chainr pExprSimple (ExprOper <$> sOperator)
 
 pExpr :: Parser Token Expr
+-- pExpr = pExpr1
 pExpr =  ExprOper "="  <$> pExpr <* symbol (Operator "=")  <*> pExpr
      <|> ExprOper "||" <$> pExpr <* symbol (Operator "||") <*> pExpr
      <|> ExprOper "^"  <$> pExpr <* symbol (Operator "^")  <*> pExpr
@@ -106,6 +107,40 @@ pExpr =  ExprOper "="  <$> pExpr <* symbol (Operator "=")  <*> pExpr
      <|> ExprOper "/"  <$> pExpr <* symbol (Operator "/")  <*> pExpr
      <|> ExprOper "%"  <$> pExpr <* symbol (Operator "%")  <*> pExpr
      <|> pExprSimple
+
+-- op1 :: Parser Token Expr
+-- op1, op2, op3, op4, op5, op6, op7, op8, op9, op10, op11, op12, op13, op14, op15 :: Parser Token (Expr->Expr->Expr)
+-- op1 = ExprOper "=" <$> symbol (Operator "=") <*> empty
+-- op2 = ExprOper "||" <$> symbol (Operator "||") <*> empty
+-- op3 = ExprOper "^"  <$> symbol (Operator "^")  <*> empty
+-- op4 = ExprOper "&&" <$> symbol (Operator "&&") <*> empty
+-- op5 = ExprOper "<=" <$> symbol (Operator "<=") <*> empty
+-- op6 = ExprOper "<"  <$> symbol (Operator "<")  <*> empty
+-- op7 = ExprOper ">=" <$> symbol (Operator ">=") <*> empty
+-- op8 = ExprOper ">"  <$> symbol (Operator ">")  <*> empty
+-- op9 = ExprOper "==" <$> symbol (Operator "==") <*> empty
+-- op10 = ExprOper "!=" <$> symbol (Operator "!=") <*> empty
+-- op11 = ExprOper "-"  <$> symbol (Operator "-")  <*> empty
+-- op12 = ExprOper "+"  <$> symbol (Operator "+")  <*> empty
+-- op13 = ExprOper "*"  <$> symbol (Operator "*")  <*> empty
+-- op14 = ExprOper "/"  <$> symbol (Operator "/")  <*> empty
+-- op15 = ExprOper "%"  <$> symbol (Operator "%")  <*> empty
+-- pExpr1, pExpr2, pExpr3, pExpr4, pExpr5, pExpr6, pExpr7, pExpr8, pExpr9, pExpr10, pExpr11, pExpr12, pExpr13, pExpr14, pExpr15 :: Parser Token Expr
+-- pExpr1  = chainr pExpr2 op1
+-- pExpr2 = chainl pExpr3 op2
+-- pExpr3 = chainl pExpr4 op3
+-- pExpr4 = chainl pExpr5 op4
+-- pExpr5 = chainl pExpr6 op5
+-- pExpr6 = chainl pExpr7 op6
+-- pExpr7 = chainl pExpr8 op7
+-- pExpr8 = chainl pExpr9 op8
+-- pExpr9 = chainl pExpr10 op9
+-- pExpr10 = chainl pExpr11 op10
+-- pExpr11 = chainl pExpr12 op11
+-- pExpr12 = chainl pExpr13 op12
+-- pExpr13 = chainl pExpr14 op13
+-- pExpr14 = chainl pExpr15 op14
+-- pExpr15 = chainl pExprSimple op15
 
 pDecl :: Parser Token Decl
 pDecl = Decl <$> pType <*> sLowerId
