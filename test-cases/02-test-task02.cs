4a822e877b16ae5a3b61252be6ec895a84d75847
Amberbm <56920715+Amberbm@users.noreply.github.com>
2022-01-28 16:38:15 +0100

    undo commited changes to test file

--- test-cases/02-test-task02.cs
+++ test-cases/02-test-task02.cs
@@ -1,11 +1,8 @@
 class Hello {
     void main() {
-        // print(3 * 5 + 2);
+        print(3 * 5 + 2);
 
-        // bool i;
-        // i = 3 + 2 == 1;
-        
-        1 == 1 || true;
+        print(3 + 2 == 1);
     }
 }
 
